b2ece443f1e0a7e3f60ca0c21ceb3a11089f7ac5
julienpelletiermorin@gmail.com <julienpelletiermorin@gmail.com@2efb422c-0880-7100-c2cf-b09a9d08a44d>
2013-03-23 23:22:16 +0000

    both scripts to create and drop tables (incomplete)

--- trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DDL_create_tables.sql
+++ trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DDL_create_tables.sql
@@ -1,6 +1,12 @@
 --
 -- Tables with no dependancies
 --
+CREATE TABLE tags
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50) NOT NULL
+,url VARCHAR(200) NOT NULL
+);
+
 CREATE TABLE adresses
 (id SERIAL PRIMARY KEY
 ,no_civique INTEGER NOT NULL
@@ -11,24 +17,97 @@ CREATE TABLE adresses
 ,province VARCHAR(2) NOT NULL
 );
 
+CREATE TABLE types_spectacle
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50) NOT NULL
+,description VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE types_billet
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50) NOT NULL
+,description VARCHAR(300) NOT NULL
+);
+
 CREATE TABLE commandes
 (id SERIAL PRIMARY KEY
 ,date_creation TIMESTAMP NOT NULL
 );
 
+CREATE TABLE nouvelles
+(id SERIAL PRIMARY KEY
+,titre VARCHAR(50) NOT NULL
+,description VARCHAR(300) NOT NULL
+,date DATE NOT NULL
+,auteur VARCHAR(50) NOT NULL
+);
+
+
+
+
 --
 -- Tables with dependancies
 --
+
+
+
+
 CREATE TABLE item_commandes
 (id SERIAL PRIMARY KEY
 ,commande_id INTEGER NOT NULL REFERENCES commandes(id)
 ,quantite INTEGER CHECK(quantite > 0)
 );
 
-CREATE TABLE transactions
+CREATE TABLE clients
 (id SERIAL PRIMARY KEY
 ,commande_id INTEGER NOT NULL REFERENCES commandes(id)
-,nom VARCHAR(255)
-,no_confirmation_paiement INTEGER
-,total_transaction NUMERIC
+,identifiant VARCHAR(50) NOT NULL
+,mot_de_passe VARCHAR(50) NOT NULL
+,nom VARCHAR(50) NOT NULL
+,email VARCHAR(100) NOT NULL
+,adresse_id VARCHAR(100) NOT NULL REFERENCES adresses(id)
+);
+
+CREATE TABLE spectacles
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50)NOT NULL
+,capacite INTEGER CHECK(quantite > 0) 
+,adresse_id INTEGER NOT NULL REFERENCES adresse_(id)
+,type_spectacle INTEGER NOT NULL REFERENCES types_spectacle(id)
+);
+
+CREATE TABLE artistes
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50) NOT NULL
+,description VARCHAR(300) NOT NULL
+,spectacle_id INTEGER NOT NULL REFERENCES spectacles(id)
+);
+
+CREATE TABLE types_billet_representation
+(id SERIAL PRIMARY KEY
+,prix NUMERIC NOT NULL
+,nb_billet_emis INTEGER NOT NULL
+,type_billet_id INTEGER NOT NULL REFERENCES types_billet(id)
+);
+
+CREATE TABLE salles
+(id SERIAL PRIMARY KEY
+,nom VARCHAR(50)NOT NULL
+,capacite INTEGER NOT NULL CHECK(capacite > 0) 
+,adresse_id INTEGER NOT NULL REFERENCES adresses(id)
+);
+
+CREATE TABLE representations
+(id SERIAL PRIMARY KEY
+,statut VARCHAR(50) NOT NULL
+,salle INTEGER NOT NULL REFERENCES salles(id) 
+,date_debut DATE NOT NULL
+,date_fin DATE NOT NULL
+);
+
+CREATE TABLE transactions
+(id serial PRIMARY KEY
+,nom VARCHAR(50) NOT NULL
+,numero_confirmation_paiement INTEGER NOT NULL
+,total_transaction NUMERIC NOT NULL,
 );
--- trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DDL_drop_tables.sql
+++ trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DDL_drop_tables.sql
@@ -6,7 +6,23 @@ DROP SEQUENCE commande_id_seq;
 
 
 -- DROP TABLES --
+DROP TABLE tags;
+DROP TABLE adresses;
+DROP TABLE item_commandes;
 DROP TABLE commandes;
+DROP TABLE clients;
+DROP TABLE artistes;
+DROP TABLE nouvelles;
+DROP TABLE types_spectacle;
+DROP TABLE types_billet;
+DROP TABLE types_billet_representation;
+DROP TABLE types_spectacle;
+DROP TABLE salles;
+DROP TABLE representations;
+DROP TABLE spectacles;
+
+
+
 
 
 
