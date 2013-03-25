631ecae82dd707e40b4f5e95b205ce05024086d7
lavalleep <lavalleep@2efb422c-0880-7100-c2cf-b09a9d08a44d>
2013-03-25 23:34:26 +0000

    Mange dla marde avec tes cheveux long de princesse!

---
+++ trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DDL_create_database.sql
@@ -0,0 +1,11 @@
+-- Database: "SPORACID"
+
+-- DROP DATABASE "SPORACID";
+
+CREATE DATABASE "SPORACID"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
