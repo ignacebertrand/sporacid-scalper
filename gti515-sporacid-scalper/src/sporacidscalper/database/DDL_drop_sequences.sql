566d2ab1c1e3c1cd2a56c71af3a4807a828a9fe3
lavalleep <lavalleep@2efb422c-0880-7100-c2cf-b09a9d08a44d>
2013-03-26 01:10:03 +0000

    asfgjklkhd

---
+++ gti515-sporacid-scalper/src/sporacidscalper/database/DDL_drop_sequences.sql
@@ -0,0 +1,17 @@
+-- DROP SEQUENCES --
+\echo '### DROPPING SEQUENCES ###'
+DROP SEQUENCE adresse_id_seq;
+DROP SEQUENCE artiste_id_seq;
+DROP SEQUENCE client_id_seq;
+DROP SEQUENCE commande_id_seq;
+DROP SEQUENCE item_commande_id_seq;
+DROP SEQUENCE nouvelle_id_seq;
+DROP SEQUENCE representation_id_seq;
+DROP SEQUENCE salle_id_seq;
+DROP SEQUENCE spectacle_id_seq;
+DROP SEQUENCE tag_id_seq;
+DROP SEQUENCE transaction_id_seq;
+DROP SEQUENCE type_billet_id_seq;
+DROP SEQUENCE type_billet_representation_id_seq;
+DROP SEQUENCE type_spectacle_id_seq;
+\echo '### SEQUENCES DROPPED ###'
