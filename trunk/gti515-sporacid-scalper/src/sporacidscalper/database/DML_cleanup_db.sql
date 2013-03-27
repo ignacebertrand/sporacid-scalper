3d612f402c3e11bb090f437650067c61bab71d3d
lavalleep <lavalleep@2efb422c-0880-7100-c2cf-b09a9d08a44d>
2013-03-27 02:46:52 +0000

    Script de nettoyage des données

---
+++ trunk/gti515-sporacid-scalper/src/sporacidscalper/database/DML_cleanup_db.sql
@@ -0,0 +1,19 @@
+DELETE FROM types_billet_representations;
+DELETE FROM artistes_spectacles;
+DELETE FROM artistes_tags;
+DELETE FROM transactions;
+DELETE FROM spectacles;
+DELETE FROM clients;
+DELETE FROM representations;
+DELETE FROM salles;
+DELETE FROM item_commandes;
+DELETE FROM commandes;
+DELETE FROM tags;
+DELETE FROM artistes;
+DELETE FROM nouvelles;
+DELETE FROM types_spectacle;
+DELETE FROM types_billet;
+DELETE FROM adresses;
+DELETE FROM statut_commandes;
+
+COMMIT;
